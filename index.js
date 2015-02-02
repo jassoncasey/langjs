ba3c2b1f61d34cfd0d3a857b5b2931c0d4ff98e1
Jasson Casey <jasson@flowgrammable.com>
2015-02-02 07:55:49 -0500

    Adjustments to syntax test harness

--- index.js
+++ index.js
@@ -27,20 +27,21 @@ pgm
   .description('A nodejs based Modulular language experimentation framework')
   .parse(process.argv);
 
+// Force the selection of a language
 if(!_(pgm.lang).isString()) {
   console.log('Must specify a language');
+// Envoke the language driver
 } else {
-
-drv.drive({
-  language: pgm.lang,
-  plugins: pgm.plugin,
-  display: {
-    source:      pgm.source      ? true : false,
-    syntax:      pgm.syntax      ? true : false,
-    typing:      pgm.typing      ? true : false,
-    evaluation:  pgm.evaluation  ? true : false,
-    compilation: pgm.compilation ? true : false
-  }
-}, pgm.args);
-
+  drv.drive({
+    language: pgm.lang,
+    plugins: pgm.plugin,
+    display: {
+      source:      pgm.source      ? true : false,
+      syntax:      pgm.syntax      ? true : false,
+      typing:      pgm.typing      ? true : false,
+      evaluation:  pgm.evaluation  ? true : false,
+      compilation: pgm.compilation ? true : false
+    }
+  }, pgm.args);
 }
+
--- test/syntax.js
+++ test/syntax.js
@@ -4,30 +4,38 @@ var path   = require('path');
 var _      = require('underscore');
 var expect = require('chai').expect;
 
-var trans  = require('../lib/translations');
+var trans  = require('../lib/translation');
 var parser = require('../lib/parser');
 var err    = require('../lib/error');
 
 describe('Possitive and negative syntax testing', function() {
   var dir = path.join(__dirname, 'syntax');
-  _(fs.readdirSync(dir)).each(function(file) {
-    var ext = path.extname(file);
-    if(ext === '.pass') {
-      it('Positive syntax test case: ' + file, function() {
-        var src = new trans.Source(path.join(dir, file));
-        // Expect test case to pass
-        try {
-          parser.parse(src);
-        } catch(e) {
-          console.log(e);
-          throw e;
+  _(fs.readdirSync(dir)).each(function(lang) {
+    var langdir = path.resolve(dir, lang);
+    if(fs.statSync(langdir).isDirectory()) {
+      var grammar = path.resolve(__dirname, '../lang/', lang, 'grammar.js');
+      _(fs.readdirSync(langdir)).each(function(file) {
+        var ext = path.extname(file);
+        if(ext === '.pass') {
+          it('Positive syntax test case: ' + file, function() {
+            var src = new trans.Source(path.resolve(langdir, file));
+            // Expect test case to pass
+            try {
+              var p = new parser.Parser(grammar);
+              p.parse(src);
+            } catch(e) {
+              console.log(e);
+              throw e;
+            }
+          });
+        } else if(ext === '.fail') {
+          it('Negative syntax test case: ' + file, function() {
+            var src = new trans.Source(path.resolve(langdir, file));
+            // Expect test case to fail
+            var p = new parser.Parser(grammar);
+            expect(parser.parse.bind(parser, src)).to.throw();
+          });
         }
-      });
-    } else if(ext === '.fail') {
-      it('Negative syntax test case: ' + file, function() {
-        var src = new trans.Source(path.join(dir, file));
-        // Expect test case to fail
-        expect(parser.parse.bind(parser, src)).to.throw();
       });
     }
   });
