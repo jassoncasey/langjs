72229fc1c3ddc29110a9dcf082007266e8d2b49e
Jasson Casey <jasson@flowgrammable.com>
2015-02-05 08:37:37 -0500

    Cleaning up ir and type definitions

--- lang/steve/ir.js
+++ lang/steve/ir.js
@@ -3,56 +3,61 @@
 
 var _ = require('underscore');
 var pr = require('./printer');
+var types = require('./types');
+
+function noop() {}
 
 function Visitor() {}
 
+
+/*
+ * Constant - constant/literal in the language
+ *
+ * Examples: 1, -1, true, 'c', 'im a literal', ...
+ */
 function Constant(name, value) {
   this.name  = name;
   this.value = value;
 }
 
+// Visitor callback
 Constant.prototype.accept = function(v) {
   v.constant(this);
 };
 
-Visitor.prototype.constant = function() {};
+// Generic constant visitor
+Visitor.prototype.constant = noop;
 
+/*
+ * Variable - symbol referencing a value
+ *
+ * Examples: x, foo, dostuff, ...
+ */
 function Variable(name) {
   this.name = name;
 }
 
+// Visitor callback
 Variable.prototype.accept = function(v) {
   v.variable(this);
 };
 
-Visitor.prototype.variable = function() {};
+// Generic variable visitor
+Visitor.prototype.variable = noop;
 
-function SimpleType(name) {
-  this.name = name;
-}
+// Import the simple type
+var SimpleType               = types.SimpleType;
+Visitor.prototype.simpleType = types.visitSimpleType;
 
-SimpleType.prototype.accept = function(v) {
-  v.simpleType(this);
-};
+// Import the arrow type
+var ArrowType               = types.ArrowType;
+Visitor.prototype.arrowType = types.visitArrow;
 
-Visitor.prototype.simpleType = function() {};
-
-function ArrowType(argTypes, retType) {
-  this.argTypes = argTypes;
-  this.retType  = retType;
-}
-
-ArrowType.prototype.accept = function(v) {
-  v.arrowType(this);
-};
-
-Visitor.prototype.arrowType = function(arrow) {
-  _(arrow.argTypes).each(function(arg) {
-    arg.accept(this);
-  });
-  arrow.retType.accept(this);
-};
-
+/*
+ * Seq - sequence of expressions
+ *
+ * Example: 1; 1+2; dostuff(1, 2);
+ */
 function Seq(expr) {
   if(_(expr).isArray()) {
     this.exprs = expr;
@@ -63,83 +68,124 @@ function Seq(expr) {
   }
 }
 
+// Add an expression to the sequence
 Seq.prototype.push = function(expr) {
   this.exprs.push(expr);
 };
 
+// Visitor callback
 Seq.prototype.accept = function(v) {
   v.seq(this);
 };
 
+// Generic sequence visitor
 Visitor.prototype.seq = function(seq) {
   _(seq.exprs).each(function(expr) {
     expr.accept(this);
-  });
+  }, this);
 };
 
+/*
+ * BindTerm - bind a symbol to a type
+ *
+ * Examples: x: int, y: nat, z: bool
+ */
 function BindTerm(name, type) {
   this.name = name;
+  // Transform any variables to simple types
+  if(type instanceof Variable) {
+    type = new SimpleType(type.name);
+  }
   this.type = type;
 }
 
+// Visitor callback
 BindTerm.prototype.accept = function(v) {
   v.bindTerm(this);
 };
 
+// Generic bind term visitor
 Visitor.prototype.bindTerm = function(bind) {
   bind.type.accept(this);
 };
 
+/*
+ * BindType - bind a type symbol to a kind
+ *
+ * Examples: Header:: typename; Coordinates:: Pair(int, int);
+ */
 function BindType(name, kind) {
   this.name = name;
   this.kind = kind;
 }
 
+// Visitor callback
 BindType.prototype.accept = function(v) {
   v.bindType(this);
 };
 
+// Generic bind type visitor
 Visitor.prototype.bindType = function(bind) {
   bind.kind.accept(this);
 };
 
+/*
+ * BindKind - bind a kind symbol to a kind
+ *
+ * Examples: Vector::: (T:: typename) -> typename;
+ */
 function BindKind(name, kind) {
   this.name = name;
   this.kind = kind;
 }
 
+// Visitor callback
 BindKind.prototype.accept = function(v) {
   v.bindKind(this);
 };
 
+// Generic bind kind visitor
 Visitor.prototype.bindKind = function(bind) {
   bind.kind.accept(this);
 };
 
+/*
+ * Store - store an expression, type, or kind value 
+ *         with a symbol location
+ */
 function Store(name, expr) {
   this.name = name;
   this.expr = expr;
 }
 
+// Visitor callback
 Store.prototype.accept = function(v) {
   v.store(this);
 };
 
+// Generic store visitor
 Visitor.prototype.store = function(store) {
   store.name.accept(this);
   store.expr.accept(this);
 };
 
+/*
+ * Conditional - standarnd branching conditional expression
+ *
+ * Examples: if(predicate) then { expr } else { expr }
+ */
 function Conditional(pred, thenExpr, elseExpr) {
   this.pred      = pred;
   this.thenExpr = thenExpr;
   this.elseExpr = elseExpr;
 }
 
+// Visitor callback
 Conditional.prototype.accept = function(v) {
   v.conditional(this);
 };
 
+// Generic conditional visitor
 Visitor.prototype.conditional = function(conditional) {
   conditional.pred.accept(this);
   conditional.thenExpr.accept(this);
@@ -148,99 +194,148 @@ Visitor.prototype.conditional = function(conditional) {
   }
 };
 
+/*
+ * Block - lexically scoped container of expressions
+ *
+ * Example: { x+1; 1; }
+ */
 function Block(body) {
   this.body = body;
 }
 
+// Visitor callback
 Block.prototype.accept = function(v) {
   v.block(this);
 };
 
+// Generic block visitor
 Visitor.prototype.block = function(block) {
   block.body.accept(this);
 };
 
+/*
+ * While - standard while loop expression
+ *
+ * Example: while(predicate) { ... do stuff ... }
+ */
 function While(pred, body) {
   this.pred = pred;
   this.body = body;
 }
 
+// Visitor callback
 While.prototype.accept = function(v) {
   v.while_(this);
 };
 
+// Generic while visitor
 Visitor.prototype.while_ = function(while_) {
   while_.pred.accept(this);
   while_.body.accept(this);
 };
 
+/*
+ * Return - return control flow from a function 
+ *
+ * Example: return -1;
+ */
 function Return(expr) {
   this.expr = expr;
 }
 
+// Visitor callback
 Return.prototype.accept = function(v) {
   v.return_(this);
 };
 
+// Generic return visitor
 Visitor.prototype.return_ = function(return_) {
   return_.expr.accept(this);
 };
 
+/*
+ * Unary - unary operation wrapper
+ * 
+ * Examples: -1, !x, ~0x0f, ..
+ */
 function Unary(op, expr) {
   this.op   = op;
   this.expr = expr;
 }
 
+// Visitor callback
 Unary.prototype.accept = function(v) {
   v.unary(this);
 };
 
+// Generic unary visitor
 Visitor.prototype.unary = function(unary) {
   unary.expr.accept(this);
 };
 
+/*
+ * Binary - binary operation wrapper
+ *
+ * Examples: 1 + x; 0x0ff << 2; ..
+ */
 function Binary(op, lhs, rhs) {
   this.op  = op;
   this.lhs = lhs;
   this.rhs = rhs;
 }
 
+// Visitor callback
 Binary.prototype.accept = function(v) {
   v.binary(this);
 };
 
+// Generic binary visitor
 Visitor.prototype.binary = function(binary) {
   binary.lhs.accept(this);
   binary.rhs.accept(this);
 };
 
+/*
+ * Func - function expression
+ *
+ * Example: id(x: int) -> int = { return x; }
+ */
 function Func(arrow, body) {
   this.arrow = arrow;
   this.body  = body;
 }
 
+// Visitor callback
 Func.prototype.accept = function(v) {
   v.func(this);
 };
 
+// Generic function visitor
 Visitor.prototype.func = function(func) {
   func.arrow.accept(this);
   func.body.accept(this);
 };
 
+/*
+ * Call - function invocation
+ *
+ * Examples: x(), foo(1, x), ...
+ */
 function Call(name, args) {
   this.name   = name;
   this.args = args ? args : [];
 }
 
+// Visitor callback
 Call.prototype.accept = function(v) {
   v.call(this);
 };
 
+// Generic call visitor
 Visitor.prototype.call = function(call) {
   _(call.args).each(function(arg) {
     arg.accept(this);
-  });
+  }, this);
 };
 
 exports.Constant    = Constant;
---
+++ lang/steve/types.js
@@ -0,0 +1,98 @@
+(function() {
+  'use strict';
+
+function noop() {}
+
+/* 
+ * SimpleType - wrapper for ground types
+ *
+ * Examples: BOOL, CHAR, STRING, NAT, INT, ...
+ */
+
+function SimpleType(name) {
+  this.name = name;
+}
+
+// Visitor callback
+SimpleType.prototype.accept = function(v) {
+  v.simpleType(this);
+};
+
+// Type equality operation
+SimpleType.prototype.equal = function(rhs) {
+  return rhs instanceof SimpleType && this.name === rhs.name;
+};
+
+// Generic simpleType visitation
+var visitSimpleType = noop;
+
+/*
+ * RefType - wrapper type for references
+ *
+ * Examples: ref types can be implied; used for store validation
+ */
+function RefType(type) {
+  this.type = type;
+}
+
+// Visitor callback
+RefType.prototype.accept = function(v) {
+  v.refType(this);
+};
+
+// Type equality operation
+RefType.prototype.equal = function(rhs) {
+  return rhs instanceof RefType &&
+    equal(this.type, rhs.type);
+};
+
+// Generic ref type visitor
+function visitRefType(refType) {
+  refType.type.accept(this);
+}
+
+/*
+ * ArrowType - function/abstraction type
+ *
+ * Examples: < has the type ... (NAT, NAT) -> BOOL
+ */
+function ArrowType(argTypes, retType) {
+  this.argTypes = argTypes;
+  this.retType  = retType;
+}
+      
+// Visitor callback
+ArrowType.prototype.accept = function(v) {
+  v.arrowType(this);
+};
+
+// Type equality operation
+ArrowType.prototype.equal = function(rhs) {
+  return rhs instanceof ArrowType &&
+    this.argTypes.length === rhs.argTypes.length &&
+    _(_(this.argTypes, rhs.argTypes).zip()).every(function(argPair) {
+      return equal(argPair[0], argPair[1]); 
+    }) && equal(this.retType, rhs.retType);
+};
+
+// Generic arrow visitation
+function visitArrow(arrow) {
+  _(arrow.argTypes).each(function(arg) {
+    arg.accept(this);
+  }, this);
+  arrow.retType.accept(this);
+}
+
+// Entry point for type equality checks
+function equal(lhs, rhs) {
+  return lhs.equal(rhs);
+}
+
+exports.SimpleType      = SimpleType;
+exports.visitSimpleType = visitSimpleType;
+exports.ArrowType       = ArrowType;
+exports.visitArrow      = visitArrow;
+exports.equal           = equal;
+
+})();
+
